bf02b37d79b282b6b5d9275d6a97b4af6ca624a4
SeratnaBass <tomamichibasket106@gmail.com>
2023-09-17 20:13:50 +0900

    lowpass filterのコメントアウトを解除

--- PitchCalc.js
+++ PitchCalc.js
@@ -22,8 +22,8 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
 
     analyserNode.fftSize = 16384;
     gainNode.gain.value = 0.6;
-    // biquadFilterNode.type = 'lowpass';
-    // biquadFilterNode.frequency.value = 1500; // ギターなどのチューニングに用いる場合のフィルタ
+    biquadFilterNode.type = 'lowpass';
+    biquadFilterNode.frequency.value = 1500; // ギターなどのチューニングに用いる場合のフィルタ
 
     let freq = 1024;
 
